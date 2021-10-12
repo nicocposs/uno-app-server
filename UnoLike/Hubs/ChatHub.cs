a649afbc9bcdb190d96ae8797c23897408db33ba
nicocposs <msnjus@hotmail.fr>
2021-10-12 22:07:22 +0200

    Ajoutez des fichiers projet.

---
+++ UnoLike/Hubs/ChatHub.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.SignalR;                                       
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnoLike.Hubs
+{
+    public class ChatHub : Hub                                           
+    {
+        public static int nbrOfPlayers;
+        public override Task OnConnectedAsync()
+        {
+            nbrOfPlayers++;
+            SendConnectedState();
+            if(nbrOfPlayers == 4)
+            {
+                SendHasEnoughPlayers();
+            }
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            nbrOfPlayers--;
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public Task SendConnectedState()
+        {
+            Console.WriteLine(Context.ConnectionId);
+            return Clients.Client(Context.ConnectionId).SendAsync("ConnectedState", true);
+        }
+
+        public Task SendHasEnoughPlayers()
+        {
+            return Clients.All.SendAsync("HasEnoughPlayers", true);
+        }
+
+        public Task SendName(string name)              
+        {
+            return Clients.All.SendAsync("PlayerName", name);
+        }
+    }
+}
